75a28ecaa4e6e62ca5b2616711d207d0a936df9e
Marix27 <mariajosepardoruiz18@gmail.com>
2024-12-05 19:13:39 +0000

    actualizar pagina

--- paginas/inputs.py
+++ paginas/inputs.py
@@ -8,8 +8,8 @@ st.header("selectbox from a list")
 contact_selected = st.selectbox("why would you like to be contacted?",
                                 options = contact_options)
 
-st.write("selectbox returns:", contact selected,
-          of type, type(contact_selected))
+st.write("selectbox returns:", contact_selected,
+          "of type", type(contact_selected))
 
 if contact_selected == "Email":
     st.write("**confirm your email address by clicking the link sen to you**")
