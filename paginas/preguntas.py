be377ff2a605d48611ca4b34f044177e272d5649
Marix27 <mariajosepardoruiz18@gmail.com>
2024-12-06 17:10:55 +0000

    actualizar pagina

--- paginas/preguntas.py
+++ paginas/preguntas.py
@@ -7,10 +7,8 @@ import pandas as pd
 #graficar figuras geometricas
 st.divider()
 st.subheader("Figuras geometricas")
-c1, c2 ,c3, c4 = st.columns([1, 3, 5, 7], vertical_alignment="center")
+c1, c2 = st.columns([1, 3], vertical_alignment="center")
 puntos = pd.DataFrame({
-    "x": [1, 1, -1],
-    "y": [1, -1, 0.5],
     "x": [1, 1, -1],
     "y": [1, -1, 0.5],
  })
@@ -30,7 +28,15 @@ with c2:
 
 st.divider()
 
-with c3:
+st.title("aplicar")
+st.divider()
+st.subheader("Figuras geometricas")
+c1, c2 = st.columns([1, 3], vertical_alignment="center")
+puntos = pd.DataFrame({
+    "x": [1, 1, -1],
+    "y": [1, -1, 0.5],
+ })
+with c1: 
     
   puntos = st.data_editor(puntos, hide_index= True, num_rows="dynamic")
 pol = patches.Polygon(puntos, closed= True, fill=True, facecolor="skyblue", edgecolor="navy")
@@ -41,10 +47,8 @@ ax.add_patch(pol)
 ax.set_xlim(puntos["x"].min()-1, puntos["x"].max()+1)
 ax.set_ylim(puntos["y"].min()-1, puntos["y"].max()+1)
 ax.set_aspect("equal")
-
-with c4:
-  st.pyplot(fig)
+with c2: 
+ st.pyplot(fig)
 
 st.divider()
-
 
