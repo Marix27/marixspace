f85243616dd82b190e1e0c6c8200c9920a4470d1
Marix27 <mariajosepardoruiz18@gmail.com>
2024-12-06 03:25:18 +0000

    actualizar pagina

--- paginas/inputs.py
+++ paginas/inputs.py
@@ -6,15 +6,15 @@ rta2 = st.selectbox("elegir", ["si","no"])
 c2 = st.empty
 
 if st.button("verificar"):
-     ptos = 0
-     if rta1 == "no":
+    ptos = 0
+    if rta1 == "no":
           c1.info("perfecto")
           ptos += 1
-     else:
+    else:
           c1.error("incorrecto")
 
         
-     if rta2 == "arquimedes"
+    if rta2 == "arquimedes"
           c2.info("perfecto")
           ptos += 1
     else:
